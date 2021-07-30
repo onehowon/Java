e3fa9b2cd2b0120ede14b9abf52479c64d7bbfc0
onehowon <flash0221@naver.com>
2021-07-30 22:09:50 +0900

    Ex 2 예제

---
+++ src/Ex2_4.java
@@ -0,0 +1,11 @@
+
+public class Ex2_4 {
+	public static void main(String args[]) {
+		int x = 5; // int x;와 x=5를 한줄로 합칠 수 있음
+		System.out.println(x); // 화면에 5 출력
+		
+		x = 10; // 변수 x에 5대신 10을 저장되고 기존에 저장된 5는 제거됨
+		System.out.println(x);
+	}
+
+}
