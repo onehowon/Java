30c9ff97f6e336772b505827bc30d68c8c1bb3fc
성원호 <flash0221@ajou.ac.kr>
2022-08-25 01:39:41 +0000

    0825

--- src/baekjoon/blackjack.java
+++ src/baekjoon/blackjack.java
@@ -10,10 +10,30 @@ public class blackjack{
         
         int[] arr = new int[N];
         
-        for(int i =0l i < N; i++){
+        for(int i =0; i < N; i++){
             arr[i] = in.nextInt();
         }
         int result = search(arr, N, M);
         System.out.println(result);
+    }
+    
+    static int search(int[] arr, int N, int M){
+        int result = 0;
+        
+        // 카드가 3장이기에 N-2 ~ N까지 3번 반복
+        for(int i=0; i<N-2; i++){
+            for(int j = i+1; j<N-1; j++){
+                for(int k=j+1; k<N; k++){
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if(M == temp){
+                        return temp;
+                    }
+                    if(result < temp && temp < M){
+                        result = temp;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
