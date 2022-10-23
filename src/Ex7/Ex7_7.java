5da6a1fd9cc0b9585155fe005fd912495ed86966
성원호 <flash0221@ajou.ac.kr>
2022-10-23 04:03:00 +0000

    10.23

---
+++ src/Ex7/Ex7_7.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class Ex7_7{
+    public static void main(String[] args){
+        out.println(random());
+        
+        out.println("Math.PI :" + PI);
+    }
+}
