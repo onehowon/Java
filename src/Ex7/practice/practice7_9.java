13ae6f76c2e3572583031c245c40322350f2313d
성원호 <flash0221@ajou.ac.kr>
2022-11-02 13:00:23 +0000

    11.02

---
+++ src/Ex7/practice/practice7_9.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class practice7_9{
+    public static void main(String[] args){
+        Frame f = new Frame();
+        f.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                e.getWindow().setVisible(false);
+                e.getWindow().dispose();
+                System.exit(0);
+            }
+        }
+                            );
+        
+    }
+}
