4efce38a97d9b7c7b5d9b227eb32873f62b39e32
onehowon <flash0221@ajou.ac.kr>
2023-10-31 18:01:44 +0900

    java

---
+++ src/Profile.java
@@ -0,0 +1,13 @@
+public class Profile {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        int studentNumber = 2018122104;
+        double tall = 1.78;
+        boolean isMale = true;
+
+        System.out.println("이름:" + name);
+        System.out.println("학번 = " + studentNumber);
+        System.out.println("신장 = " + tall);
+        System.out.println("남자인가요= " + isMale);
+    }
+}
