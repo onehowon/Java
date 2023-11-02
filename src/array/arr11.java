ac136680f5a787fb3d900807e45378e8833279ec
onehowon <flash0221@ajou.ac.kr>
2023-11-02 16:07:47 +0900

    java

---
+++ src/array/arr11.java
@@ -0,0 +1,15 @@
+package array;
+
+import java.util.Scanner;
+
+public class arr11 {
+    public static void main(String[] args) {
+        String[] words = {"television", "computer", "mouse", "phone"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i<words.length; i++){
+            char[] question = words[i].toCharArray();
+        }
+    }
+}
