7ed351bd659fb747df9751556e1edfb17d7507af
onehowon <flash0221@ajou.ac.kr>
2023-11-07 17:52:22 +0900

    java

--- src/instanceInner2.java
+++ src/instanceInner2.java
@@ -2,7 +2,7 @@ public class instanceInner2 {
     class InstanceInner{}
     static class StaticInner{}
 
-    InstanceInner iv = new InstanceInner();
+    InstanceInner iv = new InstanceInner();전라남도 목포시 산정동 1277-5
     static StaticInner cv = new StaticInner();
 
     static void staticMethod(){
