aa7a30b3e04e384cf0c45bb2f924e7ac87d6acd8
onehowon <flash0221@ajou.ac.kr>
2023-11-09 14:19:16 +0900

    java

--- src/exception_4.java
+++ src/exception_4.java
@@ -1,18 +1,17 @@
-public class exception_5 {
+public class exception_4 {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+
         try{
             System.out.println(3);
             System.out.println(0/0);
             System.out.println(4);
         } catch(ArithmeticException ae){
-            if(ae instanceof ArithmeticException)
-                System.out.println("true");
-            System.out.println("ArithmeticException");
-        } catch(Exception e){
-            System.out.println("Exception");
+            ae.printStackTrace();
+            System.out.println("예외메시지: " + ae.getMessage());
         }
+
         System.out.println(6);
     }
 }
