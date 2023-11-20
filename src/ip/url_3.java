bcb02d30c6994289e4166a4b9cc59974ecd10de8
onehowon <flash0221@ajou.ac.kr>
2023-11-20 14:49:34 +0900

    java

--- src/ip/url_3.java
+++ src/ip/url_3.java
@@ -1,4 +1,27 @@
 package ip;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class url_3 {
+
+    public static void main(String[] args) {
+        URL url = null;
+        BufferedReader input = null;
+        String address = "http://www.codechobo.com/sample/hello.html";
+        String line = "";
+
+        try{
+            url = new URL(address);
+            input = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            while((line=input.readLine()) !=null){
+                System.out.println(line);
+            }
+            input.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
